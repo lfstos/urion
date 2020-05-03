211d1e7af5d1ca9b80e96b30513715b886053d95
Luiz Fernando <luiz-fernando@outlook.com>
2020-05-03 18:20:03 -0300

    Distância entre dois pontos

---
+++ iniciantes/distancia_entre_dois_pontos.py
@@ -0,0 +1,24 @@
+"""
+Exercício 1015
+
+Leia os quatro valores correspondentes aos eixos x e y de dois pontos quaisquer no plano, p1(x1,y1)
+e p2(x2,y2) e calcule a distância entre eles, mostrando 4 casas decimais após a vírgula, segundo a
+fórmula:
+
+Entrada
+O arquivo de entrada contém duas linhas de dados. A primeira linha contém dois valores de ponto
+flutuante: x1 y1 e a segunda linha contém dois valores de ponto flutuante x2 y2.
+
+Saída
+Calcule e imprima o valor da distância segundo a fórmula fornecida, com 4 casas após o ponto decimal.
+
+"""
+
+from math import sqrt
+
+x1, y1 = input().split(' ')
+x2, y2 = input().split()
+x1, y1, x2, y2 = float(x1), float(y1), float(x2), float(y2)
+resultado = sqrt((x2 - x1)**2 + (y2 - y1)**2)
+
+print('{:.4f}'.format(resultado))
