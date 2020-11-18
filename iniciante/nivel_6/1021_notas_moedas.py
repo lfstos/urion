6d646988a8ad45dac4d243c6c21f2ad12520026e
Luiz Fernando <luiz-fernando@outlook.com>
2020-11-18 10:03:54 -0300

    Ler um valor float e calcular o menor número de notas possíveis no qual o valor pode ser decomposto.

--- iniciante/nivel_6/1021_notas_moedas.py
+++ iniciante/nivel_6/1021_notas_moedas.py
@@ -61,16 +61,17 @@ Exemplo de Entrada      Exemplo de Saída
 '''
 
 n = float(input())
-notas = [100, 50, 20, 10, 5, 2]
-moedas = [1, 0.50, 0.25, 0.10, 0.05, 0.01]
-print('NOTAS')
-for nota in notas:
-    quantidade_nota = int(round(n, 2) / nota)
-    n -= quantidade_nota * nota
-    print('{} nota(s) de R$ {:.2f}'.format(quantidade_nota, nota))
-print('MOEDAS:')
-for moeda in moedas:
-    quantidade_moeda = int(n/moeda)
-    n -= quantidade_moeda * moeda
-    print('{} moeda(s) de R$ {:.2f}'.format(quantidade_moeda, moeda))
+if n >= 0 and n <= 1000000.00:
+    notas = [100, 50, 20, 10, 5, 2]
+    moedas = [1, 0.50, 0.25, 0.10, 0.05, 0.01]
+    print('NOTAS:')
+    for nota in notas:
+        quantidade_nota = int(round(n, 2) / nota)
+        n -= quantidade_nota * nota
+        print('{} nota(s) de R$ {:.2f}'.format(quantidade_nota, nota))
+    print('MOEDAS:')
+    for moeda in moedas:
+        quantidade_moeda = int(round(n, 2) / moeda)
+        n -= quantidade_moeda * moeda
+        print('{} moeda(s) de R$ {:.2f}'.format(quantidade_moeda, moeda))
 
