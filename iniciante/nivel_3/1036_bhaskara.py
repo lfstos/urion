3e2d90c993c8942ff521bc0cc7d0d85f97934e86
Luiz Fernando <luiz-fernando@outlook.com>
2020-11-18 10:45:52 -0300

    Ler 3 valores e efetuar o cálculo das raízes da equação de Báskara

--- iniciante/nivel_3/1036_bhaskara.py
+++ iniciante/nivel_3/1036_bhaskara.py
@@ -28,12 +28,15 @@ x = -b +- sqtr(b**2 - 4 * a * c) / 2 * a
 
 from math import sqrt
 
-a, b, c = [float(x) for x in input().split(" ")]
-delta = b**2 - 4 * a * c
-r1 = (-b + sqrt(delta)) / (2 * a)
-r2 = (-b - sqrt(delta)) / (2 * a)
-print('R1 = {:.5f}'.format(r1))
-print('R2 = {:.5f}'.format(r2))
+try:
+    a, b, c = [float(x) for x in input().split()]
+    delta = b**2 - 4 * a * c
+    r1 = (-b + sqrt(delta)) / (2 * a)
+    r2 = (-b - sqrt(delta)) / (2 * a)
+    print('R1 = {:.5f}'.format(r1))
+    print('R2 = {:.5f}'.format(r2))
+except:
+    print('Impossivel calcular')
 
 
 
